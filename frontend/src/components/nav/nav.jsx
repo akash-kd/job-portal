b983e1a6303ebdabcd82da212cab86db5f710cbc
Sumit Kumar Jha <124793539+sumitzha@users.noreply.github.com>
2023-08-12 12:56:36 +0530

    tailwind config

--- frontend/src/components/nav/nav.jsx
+++ frontend/src/components/nav/nav.jsx
@@ -1,6 +1,6 @@
 export default function Nav() { 
     return (
-        <div>
+        <div className="w-full h-10">
             <a href='/signin'> Sign In</a>
             <a href='/signup'> Sign Up</a>
         </div>
