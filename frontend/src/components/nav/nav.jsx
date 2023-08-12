16ce29b6b07ef28007aee64d9c1c92fa59139095
Sumit Kumar Jha <124793539+sumitzha@users.noreply.github.com>
2023-08-12 12:11:37 +0530

    routing

---
+++ frontend/src/components/nav/nav.jsx
@@ -0,0 +1,8 @@
+export default function Nav() { 
+    return (
+        <div>
+            <a href='/signin'> Sign In</a>
+            <a href='/signup'> Sign Up</a>
+        </div>
+    )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,19 @@ import App from './App.jsx'
 import { UserProvider } from './context/user_context.jsx';
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Nav from './components/nav/nav.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
+        element: <div>Home</div>,
+    },
+    {
+        path: '/signin',
+        element: <App />,
+    },
+    {
+        path: '/signup',
         element: <App />,
     },
 ])
@@ -15,6 +24,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <UserProvider>
+            <Nav />
             <RouterProvider router={router} />
         </UserProvider>
     </React.StrictMode>,
